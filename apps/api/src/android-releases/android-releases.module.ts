4b7f4156c36e5f85037887e6f47fea381b1e4c6c
huangke <1051483884@qq.com>
2026-07-31 09:07:09 +0800

    fix: wire android release auth dependencies

--- apps/api/src/android-releases/android-releases.module.ts
+++ apps/api/src/android-releases/android-releases.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { AndroidReleasesController } from './android-releases.controller';
 import { AndroidReleasesService } from './android-releases.service';
 
 @Module({
+  imports: [JwtModule.register({}), UsersModule],
   controllers: [AndroidReleasesController],
   providers: [PrismaService, AndroidReleasesService],
 })
